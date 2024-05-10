b36fa540c9622b6c4043317d952f4d9b0ba13763
Ilyas El Boujadaini <elbj.ilyas@gmail.com>
2024-05-10 10:19:44 +0200

    changement du nom du fichier real_data_correction_irregular.py + la méthode interpolation() fonctionne pour l'instant avec un pas de temps de 120 min converti en 60

--- scripts/11_real_data_correction_irregular_step.py
+++ scripts/11_real_data_correction_irregular_step.py
@@ -28,15 +28,14 @@ with open(entry_path, "r", newline='', encoding='utf-8') as csv_file:
     for _ in range(3):
         next(csv_reader)
     for line in csv_reader:
-        # Convertir la date en format datetime
+        # to convert the date to datetime format
         date = dt.datetime.strptime(line[0], "%Y-%m-%dT%H:%M:%S%z")
         if line[1] != '':
-            # Ajouter la nouvelle ligne au DataFrame
+            # Add the new line to the DataFrame
             data.loc[len(data)] = [date, int(line[1])]
         else:
-            # Si la puissance est vide, ajouter 0
+            # if the power is empty, add 0
             data.loc[len(data)] = [date, 0]
-print(type(data))
 
 time_step = (data['date'].diff() / pd.Timedelta(minutes=1)).fillna(0)
 data['time_step'] = [0]+time_step
@@ -48,18 +47,20 @@ def interpolation(data: pd.DataFrame, ind_step:int, wanted_step: int):
     """Data interpolation for irregular time step.
 
     Args:
-        data (pd.DataFrame): pandas Dataframe with the data to interpolate.
+        data (pd.DataFrame): pandas Dataframe with the data to interpolate, columns are.
+        ind_step (int): the index of the irregular step to correct.
         wanted_step (int): the wanted time step between each data point.
     
     Returns:
         res (pd.DataFrame): the interpolated data.
     """
     res = pd.DataFrame(columns=['date', 'power'])
-    time_difference = (data['date'][ind_step] - data['date'][ind_step-1]).total_seconds() / 60
-    x=time_difference//wanted_step-1
-    pow_scale=(data['power'][ind_step] - data['power'][ind_step-1])/(int(x)-1)
-    for i in range(int(x-1)):
-        res.loc[len(res)] = [data['date'][ind_step-1] + dt.timedelta(minutes=wanted_step*(i)), data['power'][ind_step-1]+pow_scale*(i)]
+    time_step=(data['date'][ind_step] - data['date'][ind_step-1]).total_seconds() / 60
+    x=time_step//wanted_step
+    print('coucou',x)
+    pow_scale=(data['power'][ind_step] - data['power'][ind_step-1])/(int(x))
+    for i in range(1,int(x)):
+        res.loc[len(res)] = [data['date'][ind_step-1] + dt.timedelta(minutes=wanted_step*i), data['power'][ind_step-1]+pow_scale*(i)]
     return res
 
 df=interpolation(data, 161, 60)
