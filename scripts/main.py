90807382b338417685086ff8072b5c8e8c589d15
Ilyas El Boujadaini <elbj.ilyas@gmail.com>
2024-05-24 17:19:14 +0200

    debut de la docu et réalisation d'un executable, pour l'instant ne marche que avec des pas de temps de 60 min

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,19 @@
 import os
 import sys
 import pandas as pd
+import argparse
 
+# Ajoutez les répertoires nécessaires au chemin système
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 tools_dir = os.path.join(parent_dir, 'tools')
 sys.path.append(tools_dir)
+
+# Importez vos modules
 import correction as crct  # type: ignore
 import delimiter as dlmt  # type: ignore
 
 
-def main(file_name: str):
+def main(file_name: str, timestep: int):
     entry_path = os.path.join('input', file_name + '.csv')
     delimiter = dlmt.detect_delimiter(entry_path)
     data = pd.read_csv(entry_path, sep=delimiter, header=2)
@@ -23,16 +27,23 @@ def main(file_name: str):
     # We call the main function, verify the time step between each
     # data point and export the final result in a csv file
 
-    data_corrected = crct.dataset_correction(data, 60)
+    data_corrected = crct.dataset_correction(data, timestep)
     data_corrected['pas_temps'] = [0] + (data_corrected['date'].diff()
                                          / pd.Timedelta(minutes=1)).fillna(0)
-    step_change = data_corrected[data_corrected['pas_temps'] != data_corrected[
-        'pas_temps'].shift()]
+    step_change = data_corrected[data_corrected['pas_temps']
+                                 != data_corrected['pas_temps'].shift()]
     print(step_change)
     exit_path = os.path.join('output', file_name + '_cleaned.csv')
     data_corrected.to_csv(exit_path, sep=',', index=False)
 
 
 if __name__ == '__main__':
-    file_name = 'Enedis_SGE_HDM_A0622ANW'
-    main(file_name)
+    parser = argparse.ArgumentParser(description='Process and clean '
+                                     'a CSV file.')
+    parser.add_argument('file_name', type=str, help='nom ficher csv sans '
+                        'extension')
+    parser.add_argument('timestep', type=int, help='pas temporel en minutes')
+
+    args = parser.parse_args()
+
+    main(args.file_name, args.timestep)
