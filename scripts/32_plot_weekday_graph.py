245ba3e2f7f8691d39689cb9b827df9b0676c271
Ilyas El Boujadaini <elbj.ilyas@gmail.com>
2024-06-14 09:46:53 +0200

    suppression des points dans les graphiques

--- scripts/32_plot_weekday_graph.py
+++ scripts/32_plot_weekday_graph.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import locale
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Set locale for day names in French
 locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
@@ -12,57 +11,50 @@ file_name = r'output\Enedis_SGE_HDM_A06229H0_cleaned.csv'
 # Load data
 data = pd.read_csv(file_name, sep=',')
 data['date'] = pd.to_datetime(data['date'])
-data['puissance_w']  # Convert power to kWh
-
-# Extract dates without time
-data['date_sans_heure'] = data['date'].dt.date
-
-# Group data by date without time
-grouped_data = data.groupby('date_sans_heure')
-
-# Calculate the sum of 'puissance_w' for each group
-somme_puissance_par_date = grouped_data['puissance_w'].sum()
-
-# Reset the index to get a DataFrame
-data_week = somme_puissance_par_date.reset_index()
-
-# Convert 'date_sans_heure' to datetime type
-data_week['date'] = pd.to_datetime(data_week['date_sans_heure'])
+data['puissance_kw'] = data['puissance_w'] / 1000
 
 # Extract the day of the week in French
-data_week['jour_semaine'] = data_week['date'].dt.strftime('%A')
-data_week['energie_kwh'] = data_week['puissance_w'] / 1000
+data['jour_semaine'] = data['date'].dt.strftime('%A')
+data['heure'] = data['date'].dt.strftime('%H:%M:%S')
 
-date1 = data_week['date'].min().strftime('%d/%m/%Y')
-date2 = data_week['date'].max().strftime('%d/%m/%Y')
+# Group the data by 'jour_semaine' and 'heure' columns and calculate the mean
+# of 'puissance_w'
+weekly_data = data.groupby(['jour_semaine', 'heure'])[
+    'puissance_kw'].mean().reset_index()
+mean_data = weekly_data.groupby('heure')['puissance_kw'].mean().reset_index()
 
-del data_week['date_sans_heure']
-del data_week['puissance_w']
-
-print(data_week.head(20))
-
-# Define the order of days in French
-jours_semaine_order = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi',
-                       'samedi', 'dimanche']
-
-# Convert 'jour_semaine' to a categorical type with the specified order
-data_week['jour_semaine'] = pd.Categorical(
-    data_week['jour_semaine'], categories=jours_semaine_order, ordered=True)
-
-# Plotting the graph
+# Create a figure and axis
 plt.figure(figsize=(12, 8))
+ax = plt.subplot(111)
 
-# Use seaborn to plot the data
-sns.lineplot(data=data_week, x='date', y='energie_kwh', hue='jour_semaine',
-             marker='o', hue_order=jours_semaine_order)
+# Define colors for each day of the week
+colors = ['red', 'orange', 'yellow', 'pink', 'purple', 'cyan', 'blue']
 
-# Set plot title and labels
-plt.title(f"Consommation d'énergie (kWh) par jour de la semaine\n"
-          f"Du {date1} au {date2}")
-plt.xlabel('Date')
-plt.ylabel('Énergie (kWh)')
-plt.legend(title='Jour de la semaine')
+# Iterate over each day of the week
+for i, day in enumerate(['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi',
+                         'samedi', 'dimanche']):
+    # Filter data for the current day
+    day_data = weekly_data[weekly_data['jour_semaine'] == day]
+    # Plot the points and connect them with lines
+    ax.plot(day_data['heure'], day_data['puissance_kw'],
+            linestyle='-', color=colors[i], label=day)
+
+# Plot mean_data with thicker black line
+ax.plot(mean_data['heure'], mean_data['puissance_kw'], color='black',
+        linestyle='-', linewidth=4, label='Moyenne Générale')
+
+# Set labels and title
+plt.xlabel('Heure')
+plt.ylabel('Puissance Moyenne (kW)')
+plt.title('Profil de la Puissance Moyenne par Heure pour Chaque Jour de'
+          'la Semaine')
+plt.xticks(rotation=45)
 plt.grid(True)
 
-# Show the plot
+# Add legend
+plt.legend()
+
+# Save and show the plot
+plt.tight_layout()
+plt.savefig('plots/progil_puissance.png')
 plt.show()
